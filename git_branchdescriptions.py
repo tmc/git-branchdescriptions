6c38e1616a9f96f2a5b49bf7410f3573e4932f6b
Travis Cline <travis.cline@gmail.com>
2009-04-01 11:50:58 -0500

    moved to optparse

--- git_branchdescriptions.py
+++ git_branchdescriptions.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 import os
 import sys
+import re
+import optparse
 import git
+
+__version__ = (0, 0, 1)
 
 class BranchDescriptions(object):
 
-    def __init__(self, repo_path=None):
+    def __init__(self, repo_path=None, options=None, args=None):
         if not repo_path:
             repo_path = os.getcwd()
         self.repo = git.Repo(repo_path)
+        self.options = options
+        self.args = args
 
     def set(self, name, value=None):
         if not value:
@@ -25,36 +31,44 @@ class BranchDescriptions(object):
         return self.repo.git.execute(('git config --get branchdescriptions.%s' % name).split(), with_exceptions=False)
 
     def show(self, *args):
-        branches = self.repo.git.execute(['git', 'branch', '-v']).split('\n')
-        for branch in branches:
-            name = branch.strip('*').strip().split()[0]
-            desc = self.get(name)
-            if desc:
-                branch = '%s - %s' % (branch, desc)
-            print branch
+        command = ['git', 'branch']
+        if self.options.verbosity:
+            command.append('-v')
+        if self.options.remotes:
+            command.append('-r')
+        if self.options.all:
+            command.append('-a')
+        branches = self.repo.git.execute(command).split('\n')
 
-def usage():
-    return """%s cmd""" % __file__
+        for branch in branches:
+            if branch:
+                name = branch.strip('*').strip().split()[0]
+                desc = self.get(name)
+                if desc:
+                    branch = '%s - %s' % (branch, desc)
+                print branch
+
+
+    def run(self):
+        if self.options.set:
+            return self.set(' '.join(self.args))
+        else:
+            return self.show()
+
 
 if __name__ == '__main__':
 
-    handlers = {
-        1: 'show',
-        2: 'set',
-    }
+    op = optparse.OptionParser(version=__version__, option_list=(
+        optparse.Option('-v', '--verbosity', action="store_true"),
+        optparse.Option('-r', '--remotes', action="store_true"),
+        optparse.Option('-a', '--all', action="store_true"),
+        optparse.Option('-s', '--set', action="store_true"),
+    ))
 
-    handler = handlers.get(len(sys.argv), None)
+    options, args = op.parse_args()
 
-    if not handler:
-        print 'No handler for this number of arguments.'
-        print usage()
-        sys.exit(1)
+    if args and not options.set:
+        op.error('use --set to set branch description')
 
-    app = BranchDescriptions()
-    f = getattr(app, handler, None)
-
-    if not callable(f):
-        print usage()
-        sys.exit(1)
-    else:
-        sys.exit(f(*sys.argv[1:]))
+    app = BranchDescriptions(options=options, args=args)
+    sys.exit(app.run())
